f83b57a079061b73e281ee414da986bf7e5b3f43
chango <kimtothechang@gmail.com>
2023-06-25 23:19:30 +0900

    feat: 회원가입 정규표현식 추가

--- constants/signup.ts
+++ constants/signup.ts
@@ -1,7 +1,8 @@
 const signupRegExp = {
   email: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  name: /[a-zA-z0-9가-힣]{2,10}$/,
   password: /^(?=.*[a-zA-z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/,
+  name: /[a-zA-z0-9가-힣]{2,10}$/,
+  phone: /([0-9]{11})/,
 };
 
 export { signupRegExp };
