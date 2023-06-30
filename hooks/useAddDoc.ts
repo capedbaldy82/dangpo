d27aec49f6a6a395e38343a17640682f524a2fe2
chango <kimtothechang@gmail.com>
2023-07-01 07:30:46 +0900

    fix: 변수명 변경

--- hooks/useAddDoc.ts
+++ hooks/useAddDoc.ts
@@ -3,17 +3,17 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { appFireStore, timestamp } from '../firebase/config';
 
-const useAddDoc = (transaction: any, route?: string) => {
+const useAddDoc = (transaction: any, route?: string | null) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const colRef = collection(appFireStore, transaction);
   const router = useRouter();
 
-  const addDocument = async (doc: any) => {
+  const addDocument = async (data: any) => {
     setLoading(true);
     const createdTime = timestamp.fromDate(new Date());
-    const docRef = await addDoc(colRef, { ...doc, createdTime })
-      .then(() => {
+    const docRef = await addDoc(colRef, { ...data, createdTime })
+      .then((user) => {
         setLoading(false);
         if (route) router.push(route);
       })
@@ -21,6 +21,8 @@ const useAddDoc = (transaction: any, route?: string) => {
         setLoading(false);
         setError(error);
       });
+
+    return;
   };
 
   const deleteDocument = (id: any) => {};
