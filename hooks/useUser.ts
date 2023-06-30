470a51277ad1a0c1486ed3eecf5c8ee561683972
chango <kimtothechang@gmail.com>
2023-07-01 07:29:13 +0900

    feat: user doc 데이터 호출 및 저장

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,42 +1,93 @@
 'use client';
 
-import { appAuth } from '@/firebase/config';
-import { UserState } from '@/store';
+import { appAuth, appFireStore } from '@/firebase/config';
 import { onAuthStateChanged } from 'firebase/auth';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
+
+type UserDocType = {
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  address: string | null;
+  phone: string | null;
+  application: string[] | [];
+  applicationCount: number | null;
+  uid: string | null;
+  isAuthReady: boolean;
+};
 
 const useUser = () => {
-  const [isPending, setIsPending] = useState(true);
-  const [userState, setUserState] = useRecoilState(UserState);
+  const [loading, setLoading] = useState(true);
+  const [authStateLoading, setAuthStateLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [user, setUser] = useState<UserDocType>({
+    id: '',
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
+    application: [],
+    applicationCount: 0,
+    uid: '',
+    isAuthReady: false,
+  });
   const router = useRouter();
 
+  // 로그인 상태 확인 및 AuthState 설정
   useEffect(() => {
-    setIsPending(true);
     const unsubscribe = onAuthStateChanged(appAuth, (user) => {
-      if (user) {
-        if (user.displayName && user.email && user.uid) {
-          setUserState({
-            name: user.displayName,
-            email: user.email,
-            uid: user.uid,
-            isAuthReady: true,
-          });
-        }
-      } else {
+      if (!user) {
+        setLoading(false);
         router.push('/login');
+        return;
       }
 
-      setIsPending(false);
+      setUser((prev: UserDocType) => {
+        return {
+          ...prev,
+          name: user.displayName,
+          email: user.email,
+          uid: user.uid,
+          isAuthReady: true,
+        };
+      });
 
-      // setuserState({ user, userState: true });
-      // dispatch({ type: 'userState', payload: user });
+      setAuthStateLoading(false);
     });
+
     return unsubscribe;
   }, []);
 
-  return { loading: isPending, user: userState };
+  // Auth State 설정이 될 경우 Store 내 유저 정보 저장
+  useEffect(() => {
+    if (authStateLoading) return;
+
+    const myQuery = query(collection(appFireStore, 'user'), where('uid', '==', user.uid));
+    const unsubscribe = onSnapshot(
+      myQuery,
+      (snapshot: any) => {
+        snapshot.docs.forEach((doc: any) => {
+          setUser((prev: UserDocType) => {
+            return {
+              ...prev,
+              ...doc.data(),
+              id: doc.id,
+            };
+          });
+        });
+        setLoading(false);
+      },
+      (error: any) => {
+        setError(error.message);
+      }
+    );
+
+    return unsubscribe;
+  }, [authStateLoading, collection]);
+
+  return { user, loading, error };
 };
 
 export default useUser;
