a3c2b0e29bce8758d02d7734231228311b771361
chango <kimtothechang@gmail.com>
2023-07-05 08:46:14 +0900

    design: 텍스트 크기 조절

--- app/about/SubHeading.tsx
+++ app/about/SubHeading.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const SubHeading = ({ text }: Props) => {
-  return <h3 className="text-center text-5xl">{text}</h3>;
+  return <h3 className="text-center text-3xl">{text}</h3>;
 };
 
 export default SubHeading;
