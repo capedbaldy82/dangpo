d0f7c0947fda1028ee8d5c554b1fe802f3eee6ad
chango <kimtothechang@gmail.com>
2023-07-05 08:47:20 +0900

    refac: 로직 순서 변경

--- hooks/useAdmin.ts
+++ hooks/useAdmin.ts
@@ -5,6 +5,8 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
+const adminList = ['admin@gmail.com'];
+
 const useAdmin = (routing: boolean = true) => {
   const [admin, setAdmin] = useState(false);
   const [loading, setLoading] = useState(true);
@@ -13,18 +15,28 @@ const useAdmin = (routing: boolean = true) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(appAuth, (user) => {
       if (routing) {
-        if (!user) router.push('/');
-        if (user?.email !== 'admin@gmail.com') router.push('/');
-        setAdmin(true);
+        if (!user) {
+          router.push('/');
+        }
+        if (user?.email !== 'admin@gmail.com') {
+          setAdmin(false);
+          router.push('/');
+        } else {
+          setAdmin(true);
+        }
       } else {
-        if (user?.email === 'admin@gmail.com') setAdmin(true);
+        if (user?.email === 'admin@gmail.com') {
+          setAdmin(true);
+        }
       }
-
-      setLoading(false);
     });
 
     return unsubscribe;
   }, []);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [admin]);
 
   return { admin, loading };
 };
