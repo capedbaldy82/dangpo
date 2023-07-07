918a6d7132d7be15174288061f0c9d184e0f9a89
chango <kimtothechang@gmail.com>
2023-07-07 17:41:55 +0900

    feat: home 페이지 review card Firestore 호출

--- components/home/reviewList.tsx
+++ components/home/reviewList.tsx
@@ -1,32 +1,48 @@
 import Heading from '@/components/common/Heading';
 import ReviewCard from '@/components/home/reviewCard';
+import { appFireStore } from '@/firebase/config';
+import { ReviewDocType } from '@/types';
+import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 
-const ReviewList = () => {
-  const reviews = [
-    {
-      name: '김장현',
-      content: '호기심으로 신청했는데 집안이 후련해졌습니다.',
-      postId: '',
-    },
-    {
-      name: '강정규',
-      content: '평소에 시간이 없어서 신청해봤는데 기존에 두고만 있던 물건들이 팔려서 좋았습니다.',
-      postId: '',
-    },
-  ];
+const getDocument = async () => {
+  const result: ReviewDocType[] = [];
+  const transactionRef = collection(appFireStore, 'review');
+
+  const firestoreQuery = query(transactionRef, orderBy('createdTime', 'desc'), limit(2));
+
+  const querySnapshot = await getDocs(firestoreQuery);
+
+  querySnapshot.forEach((doc: any) => {
+    result.push({ ...doc.data(), id: doc.id });
+  });
+
+  return result;
+};
+
+const ReviewList = async () => {
+  const reviews = await getDocument();
+
+  // const reviews = [
+  //   {
+  //     name: '김장현',
+  //     content: '호기심으로 신청했는데 집안이 후련해졌습니다.',
+  //     postId: '',
+  //   },
+  //   {
+  //     name: '강정규',
+  //     content: '평소에 시간이 없어서 신청해봤는데 기존에 두고만 있던 물건들이 팔려서 좋았습니다.',
+  //     postId: '',
+  //   },
+  // ];
 
   return (
     <section className="w-full">
       <Heading text="고객 후기" />
       <ul className="flex space-x-4">
-        {reviews.map((card) => (
-          <ReviewCard
-            key={card.postId}
-            name={card.name}
-            content={card.content}
-            postId={card.postId + ''}
-          />
-        ))}
+        {reviews &&
+          reviews.map((card: any) => (
+            <ReviewCard key={card.id} name={card.name} content={card.content} postId={card.id} />
+          ))}
       </ul>
     </section>
   );
